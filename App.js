c2ac458a9d1370cd514faa7d06ba3cd73950869e
Ronaldo Bueno Lopes Junior <ronaldo.junior@extrabom.com.br>
2021-10-28 14:49:22 -0300

    Navegação entre login e registro

--- App.js
+++ App.js
@@ -1,77 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import logo from "./assets/images/logo.png"
+import MyStack from './src/routes';
 
 export default function App() {
     return (
-        <SafeAreaView style={styles.container}>
-            <Image source={logo} style={styles.styleimage} />
-
-            <TextInput defaultValue="LOGIN" style={styles.input}/>
-            <TextInput defaultValue="SENHA" style={styles.input}/>
-
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Conectar</Text>
-            </TouchableOpacity>
-            <View style={styles.viewDisplay}>
-                <Text>Não possuí login?</Text>
-                <TouchableOpacity style={styles.buttonCadastro}>
-                    <Text style={styles.buttonText}>cadastre aqui</Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
     );
 }
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#B0E0E6',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    styleimage: {
-        marginBottom: 30,
-    },
-    
-    viewDisplay: {
-        flexDirection: "row",
-        marginTop: 40
-    },
-
-    buttonCadastro: {
-        marginLeft: 20,
-        backgroundColor: "#FFFFFF",
-        width: "35%",
-        padding: 5,
-        borderRadius: 20
-    },
-
-    button: {
-        backgroundColor: "#F5DEB3",
-        width: "50%",
-        marginTop: 35,
-        padding: 5,
-        borderRadius: 10
-    },
-
-    buttonText: {
-        textAlign: "center",
-    },
-
-    input: {
-        width: "80%",
-        textAlign: "center",
-        justifyContent: "center",
-        marginTop: 10,
-        borderRadius: 10,
-        borderWidth: 1,
-        padding: 5
-        
-    }
-});
---
+++ src/components/Input.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, TextInput, Dimensions } from "react-native";
+
+export default function Input(prop) {
+    return (
+        <SafeAreaView>
+            <Text style={estilo.label}>{prop.label}</Text>
+            <TextInput value={prop.value}  style={estilo.input} placeholder={prop.placeholder} secureTextEntry={prop.protected == undefined ? (false) : true} />
+            
+        </SafeAreaView>
+    )
+}
+
+const windowWidth = Dimensions.get('window').width;
+
+const estilo = StyleSheet.create({
+    label: {
+        marginTop: 10,
+        width: windowWidth*0.75
+    },
+    input: {
+        width: windowWidth*0.75,
+        padding: 2,
+        textAlign: 'center',
+        borderRadius: 1,
+        borderWidth: 0.1,
+        marginTop: 5
+    }
+});
